af8d779670723940555a50746d7b2759c3f80a20
teckming93 <48541301+teckming93@users.noreply.github.com>
2019-03-18 11:58:42 +0800

    Add files via upload

--- tb.sv
+++ tb.sv
@@ -1,6 +1,7 @@
 module ram_tb;
 parameter data_width = 32;
 parameter addr_width = 10;
+parameter times_of_wr = 3;
 reg				clk;
 reg				rstn;
 reg				en;
@@ -12,6 +13,9 @@ wire	[data_width-1:0]	data_rd;
 reg	[addr_width-1:0]	x;
 
 ram dut(clk,rstn,en,wr_rdn,addr,data_wr,data_rd);
+
+reg	[addr_width-1:0]	temp_reg	[times_of_wr-1:0];
+integer				i;
 
 initial begin
 	clk = 0;
@@ -26,15 +30,17 @@ addr = 0;
 data_wr = 0;
 #10;
 rstn = 1;
-repeat (3) begin
-	x = $random;
-	write($random,x);
-	read(x);
+/*
+repeat(times_of_wr)begin
+	write_then_read;
 end
+*/
+multiple_write(times_of_wr);
 rstn = 0;
 read(x);
 rstn = 1;
-#30;
+read(x);
+#10;
 $finish;
 end
 
@@ -48,7 +54,7 @@ begin
 	wr_rdn = 1;	
 	data_wr = data_in;
 	addr = addr1;
-	$display("tb = %h data has been written into ram address %h at %0t ns ",data_wr,addr,$time);
+	$display("tb = %h data has been send to ram address %h at %0t ns ",data_in,addr,$time);
 	@(posedge clk);
 	en = 0;
 	wr_rdn = 0;
@@ -60,7 +66,7 @@ task read; //read with en
 input [addr_width-1:0]	addr2;
 begin
 	@(posedge clk);
-	en = 1;@(posedge clk);
+	en = 1;
 	wr_rdn = 0;
 	addr = addr2;	
 	@(posedge clk);
@@ -78,7 +84,30 @@ begin
 	addr = addr2;
 end
 endtask
-*/	
+*/
+
+task write_then_read;
+begin
+	x = $random;
+	write($random,x);
+	read(x);
+end
+endtask
+
+task multiple_write;
+input [times_of_wr-1:0] wr_times;
+begin
+	for(i=0;i<wr_times;i++)begin
+		x = $random;
+		write($random,x);
+		temp_reg[i] = x;
+	end
+	for(i=0;i<wr_times;i++)begin
+		read(temp_reg[i]);
+	end
+end
+endtask
+
 endmodule
 
 
